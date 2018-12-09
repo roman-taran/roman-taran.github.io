d62beed4c9fdf8ce2ed55d314b685fb60fb751b6
Roman Bludov <romus9595@gmail.com>
2018-12-09 18:55:46 +0200

    start

---
+++ precache-manifest.5835ec221f2e084690fc1bb300f2cf44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "564133d6af2c3dc88195",
+    "url": "/static/js/main.564133d6.chunk.js"
+  },
+  {
+    "revision": "25d3b20b17bbcd59d7f2",
+    "url": "/static/js/1.25d3b20b.chunk.js"
+  },
+  {
+    "revision": "564133d6af2c3dc88195",
+    "url": "/static/css/main.4dd6c836.chunk.css"
+  },
+  {
+    "revision": "96a6560866097764f67188cb59f6f816",
+    "url": "/index.html"
+  }
+];
