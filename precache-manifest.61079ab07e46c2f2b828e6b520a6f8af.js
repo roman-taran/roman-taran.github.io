fb97a92d323404bf5ad83fbd2e632c5e47c9646b
Roman Bludov <romus9595@gmail.com>
2018-12-31 18:57:41 +0200

    index.html

---
+++ precache-manifest.61079ab07e46c2f2b828e6b520a6f8af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f171e8325a416b015bb8",
+    "url": "/static/js/main.f171e832.chunk.js"
+  },
+  {
+    "revision": "08c76d4c58c5bc1aadaa",
+    "url": "/static/js/1.08c76d4c.chunk.js"
+  },
+  {
+    "revision": "f171e8325a416b015bb8",
+    "url": "/static/css/main.125a3897.chunk.css"
+  },
+  {
+    "revision": "5fa8440fad7251a523a125a7c376da81",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f7c5b107a926c66594e823127d8d542f.js"
+  "/precache-manifest.61079ab07e46c2f2b828e6b520a6f8af.js"
 );
 
 workbox.clientsClaim();
